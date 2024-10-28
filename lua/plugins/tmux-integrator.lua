5c74f8257d2fa9b7b7576bb23c5a11d25b749785
cristalcorp <cristal@cristalcorp.com>
2024-10-28 18:37:28 +0100

    tmux integration and vim-be-good

--- lua/plugins/tmux-integrator.lua
+++ lua/plugins/tmux-integrator.lua
@@ -1,6 +1,6 @@
 return {
   "christoomey/vim-tmux-navigator",
-  lazy = false,
+  --  lazy = false,
   config = function()
     vim.keymap.set("n", "<C-h>", "<cmd>TmuxNavigateLeft<CR>")
     vim.keymap.set("n", "<C-l>", "<cmd>TmuxNavigateRight<CR>")
--- lua/plugins/vim-be-good.lua
+++ lua/plugins/vim-be-good.lua
@@ -1,4 +1,4 @@
 return {
-	"ThePrimeagen/vim-be-good",
-	lazy = false,
+  "ThePrimeagen/vim-be-good",
+  --	lazy = false,
 }
