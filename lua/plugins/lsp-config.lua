0df249ace667c4f5487b9df2859f4853970f6191
cristalcorp <cristal@cristalcorp.com>
2024-10-29 18:18:23 +0100

    auto-completion

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,47 +1,55 @@
 return {
-  {
-    -- https://github.com/williamboman/mason.nvim
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end
-  },
-  {
-    -- https://github.com/williamboman/mason-lspconfig.nvim
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = {
-          "lua_ls",
-          "dockerls",
-          "gopls",
-          "jsonls",
-          "ltex",
-          "pyright",
-          "rust_analyzer",
-          "yamlls"
-        }
-      })
-    end
-  },
-  {
-    -- https://github.com/neovim/nvim-lspconfig
-    "neovim/nvim-lspconfig",
-    config = function()
-      local lspconfig = require("lspconfig")
-      --languages
-      lspconfig.lua_ls.setup({})
-      lspconfig.dockerls.setup({})
-      lspconfig.gopls.setup({})
-      lspconfig.jsonls.setup({})
-      lspconfig.ltex.setup({})
-      lspconfig.pyright.setup({})
-      lspconfig.rust_analyzer.setup({})
-      lspconfig.yamlls.setup({})
+	{
+		-- https://github.com/williamboman/mason.nvim
+		"williamboman/mason.nvim",
+		lazy = false,
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		-- https://github.com/williamboman/mason-lspconfig.nvim
+		"williamboman/mason-lspconfig.nvim",
+		lazy = false,
+		opts = {
+			auto_install = true,
+		},
+		-- config = function()
+		-- require("mason-lspconfig").setup({
+		-- ensure_installed = {
+		--"lua_ls",
+		--"dockerls",
+		--"gopls",
+		--"jsonls",
+		--"ltex",
+		--"pyright",
+		--"rust_analyzer",
+		--"yamlls",
+		--},
+		--})
+		--end,
+	},
+	{
+		-- https://github.com/neovim/nvim-lspconfig
+		"neovim/nvim-lspconfig",
+		laszy = false,
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-      vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
-    end
-  }
+			local lspconfig = require("lspconfig")
+			--languages
+			lspconfig.lua_ls.setup({ capabilities = capabilities })
+			lspconfig.dockerls.setup({ capabilities = capabilities })
+			lspconfig.gopls.setup({ capabilities = capabilities })
+			lspconfig.jsonls.setup({ capabilities = capabilities })
+			lspconfig.ltex.setup({ capabilities = capabilities })
+			lspconfig.pyright.setup({ capabilities = capabilities })
+			lspconfig.rust_analyzer.setup({ capabilities = capabilities })
+			lspconfig.yamlls.setup({ capabilities = capabilities })
+
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "je sais plus" })
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "je sais plus" })
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "code action" })
+		end,
+	},
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,32 +1,32 @@
 return {
-	-- https://github.com/nvim-treesitter/nvim-treesitter
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	config = function()
-		local config = require("nvim-treesitter.configs")
-		config.setup({
-			auto_install = true,
-			-- ensure_installed = {
-			-- "lua",
-			-- "javascript",
-			-- "python",
-			-- "rust",
-			-- "go",
-			-- "bash",
-			-- "css",
-			-- "csv",
-			-- "dockerfile",
-			-- "gitignore",
-			-- "html",
-			-- "json",
-			-- "liquid",
-			-- "markdown",
-			-- "markdown_inline",
-			-- "toml",
-			-- "yaml"
-			-- },
-			highlight = { enable = true },
-			indent = { enable = true },
-		})
-	end,
+  -- https://github.com/nvim-treesitter/nvim-treesitter
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    local config = require("nvim-treesitter.configs")
+    config.setup({
+      -- auto_install = true,
+      ensure_installed = {
+        "lua",
+        "javascript",
+        "python",
+        "rust",
+        "go",
+        "bash",
+        "css",
+        "csv",
+        "dockerfile",
+        "gitignore",
+        "html",
+        "json",
+        "liquid",
+        "markdown",
+        "markdown_inline",
+        "toml",
+        "yaml",
+      },
+      highlight = { enable = true },
+      indent = { enable = true },
+    })
+  end,
 }
